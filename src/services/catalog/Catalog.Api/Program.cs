029a8859f72e6a93b56e4a0190ce5c78300f368b
Armen Hovhannisyan <armen1998net@gmail.com>
2025-11-18 00:43:43 +0400

    feat: integrate Carter and Marten libraries into the project

--- src/services/catalog/Catalog.Api/Program.cs
+++ src/services/catalog/Catalog.Api/Program.cs
@@ -1,6 +1,23 @@
+using Carter;
+using Marten;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapScalarApiReference();
+app.MapCarter();
 
 app.Run();
